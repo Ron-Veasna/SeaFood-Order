33c97f820f5a23415e3b6f667c4106209b41185b
Ron Veasna <vaing133@gmail.com>
2023-11-22 19:21:06 +0700

    New Update

---
+++ Sna SeaFood/home.js
@@ -0,0 +1,13 @@
+function on(){
+    document.getElementById("gg").style.display="block";
+}
+function off(){
+    document.getElementById("gg").style.display="none";
+}
+function oon(){
+    document.getElementById("oo").style.display="block";
+}
+function offf(){
+    document.getElementById("oo").style.display="none";
+}
+
